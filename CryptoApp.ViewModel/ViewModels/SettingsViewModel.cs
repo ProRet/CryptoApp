d58bcca2166397eb9d656b491448064fa5ec1430
Segei Korolenko <proret228@gmail.com>
2023-03-12 16:16:40 +0200

    cat

---
+++ CryptoApp.ViewModel/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CryptoApp.Services.Services;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+
+namespace CryptoApp.ViewModel.ViewModels
+{
+    public class SettingsViewModel : ObservableObject
+    {
+        private ElementTheme _elementTheme = ThemeSelectorService.Theme;
+        private ICommand _switchThemeCommand;
+
+        public SettingsViewModel()
+        {
+        }
+
+        public ElementTheme ElementTheme
+        {
+            get => _elementTheme;
+
+            set { SetProperty(ref _elementTheme, value); }
+        }
+
+        public ICommand SwitchThemeCommand
+        {
+            get
+            {
+                if (_switchThemeCommand == null)
+                {
+                    _switchThemeCommand = new RelayCommand<ElementTheme>(
+                        async (param) =>
+                        {
+                            ElementTheme = param;
+                            await ThemeSelectorService.SetThemeAsync(param);
+                        });
+                }
+
+                return _switchThemeCommand;
+            }
+        }
+    }
+}
